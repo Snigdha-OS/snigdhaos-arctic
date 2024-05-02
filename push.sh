b28043f9d22ce364e1461a318224d4fb3ac58e86
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-02 20:20:24 +0530

    refactor(rm): removing custom git push script

--- push.sh
+++ push.sh
@@ -2,26 +2,3 @@
 
 # Author        : Eshan Roy <eshan@snigdhaos.org>
 # Author URI    : https://eshanized.github.io/
-
-# NOTE: REPO = Your Repository Name On the Host
-# NOTE: ORG =  Your ORGANIZATION NAME or USERNAME
-# NOTE: HOST = GitHub, Gitlab, Bitbucket etc.  
-
-# Repo Config
-REPO=snigdhaos-arctic
-# Host Config
-HOST=https://github.com
-# Destination Config
-ORG=Snigdha-OS
-
-# Set the repository URL and branch
-REPO_URL="${HOST}/${ORG}/${REPO}.git"
-BRANCH="master"  # or "main" depending on your repository's default branch
-
-# Commit message
-MESSAGE="⏳ @eshanized updated the repository!!!"
-
-# Add all files, commit, and push changes
-git add .
-git commit -m "$MESSAGE"
-git push origin $BRANCH
